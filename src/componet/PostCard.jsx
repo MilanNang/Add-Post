973a4a66a71f91da8e20a1c19900d1847b535303
MilanNang <milanrayka03@gmail.com>
2025-02-16 17:19:30 +0530

    modify postcard

--- src/componet/PostCard.jsx
+++ src/componet/PostCard.jsx
@@ -10,13 +10,19 @@ function PostCard({ $id, title, featuredimage, content, showContent }) {
 
   return (
     <Link to={`/post/${$id}`}>
-      <div className="w-full bg-gray-100 rounded-xl p-4 sm:p-6">
-        <div className="w-full justify-center mb-4">
-          <img src={appwriteService.getFilePreview(featuredimage)} className="rounded-xl" alt={title} />
+      <div className="w-full bg-gray-100 rounded-lg p-3 sm:p-4">
+        <div className="w-full mb-3">
+          <img
+            src={appwriteService.getFilePreview(featuredimage)}
+            className="rounded-lg w-full h-32 sm:h-40 object-cover"
+            alt={title}
+          />
         </div>
-        <h2 className="text-xl font-bold sm:text-2xl">{title}</h2>
+        <h2 className="text-sm sm:text-lg font-semibold sm:font-bold truncate">
+          {title}
+        </h2>
         {showContent && (
-          <p className="text-sm sm:text-base text-gray-700">{stripHTML(content)}</p>
+          <p className="text-xs sm:text-sm text-gray-700">{stripHTML(content)}</p>
         )}
       </div>
     </Link>
